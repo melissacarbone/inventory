bfb9ba2902aac091c21059f11d48be576aa45c7e
Melissa Carbone <mcarbone1@gmail.com>
2013-12-30 20:28:15 -0500

    add acceptance test and validations for presence of food title, description, quantity

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ActiveRecord::Base
-
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :quantity
 end
--- spec/features/user_adds_a_food_item_spec.rb
+++ spec/features/user_adds_a_food_item_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 
 feature 'user adds food item' do
-  it 'creates food item' do
-    visit '/foods/new'
+  it 'creates food item with valid attributes' do
+    visit 'foods/new'
 
     fill_in 'Title', with: 'Dumpling'
     fill_in 'Description', with: 'Yummy'
     fill_in 'Quantity', with: 10
 
     click_on 'Save Food'
-    save_and_open_page
     expect(page).to have_content('Food was successfully created')
+  end
+
+  it 'creates food item with invalid attributes' do
+    visit 'foods/new'
+
+    click_on 'Save Food'
+
+    expect(page).to have_content('Title can\'t be blank')
+    expect(page).to have_content('Description can\'t be blank')
+    expect(page).to have_content('Quantity can\'t be blank')
+
+
+
   end
 end
 
